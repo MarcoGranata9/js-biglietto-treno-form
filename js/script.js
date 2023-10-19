400f763b189366deac4f2f65f9cf9f5686ffe475
MarcoGranata9 <granatam9900@gmail.com>
2023-10-19 17:12:19 +0200

    add: event listener

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ const ticketPrice = 0.21
 
 const sendBtn = document.getElementById("sendbtn");
 
+let totalPrice 
+
+let totalPrice20
+
+let totalPrice40
+
+let message
+
 sendBtn.addEventListener("click", function () {
     // Raccolta Dati
     const distanceValue = parseInt(distance.value);
@@ -15,5 +23,18 @@ sendBtn.addEventListener("click", function () {
     console.log(distanceValue, ageValue, typeof distanceValue, typeof ageValue);
 
     // Logica
-    
+    totalPrice = distanceValue * ticketPrice
+    message = `Il prezzo del biglietto è ${totalPrice}`
+    console.log(message)
+
+    if (ageValue < 18) 
+    {
+        totalPrice20 = totalPrice - (totalPrice * 0.20);
+        message = `Il prezzo del biglietto è ${totalPrice20.toFixed(2)}`
+        console.log(message);
+    } else if (ageValue > 65) {
+        totalPrice40 = totalPrice - (totalPrice * 0.40);
+        message = `Il prezzo del biglietto è ${totalPrice40.toFixed(2)}`
+        console.log(message);
+    }
 })

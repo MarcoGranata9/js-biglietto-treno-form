24738313332124906bf8f3a6f02d47d234fdef97
MarcoGranata9 <granatam9900@gmail.com>
2023-10-20 18:33:49 +0200

    post correction commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,15 @@
-
 const distance = document.getElementById("distance");
-
 const age = document.getElementById("age");
-console.log(distance, age);
-
 const username = document.getElementById("username");
-
 const ticketPrice = 0.21;
-
 const sendBtn = document.getElementById("sendbtn");
-
 const delBtn = document.getElementById("delbtn");
-
-let totalPrice 
-
-let totalPrice20
-
-let totalPrice40
-
-let message
+const greeting = document.getElementById("greeting")
+const ticket = document.getElementById("ticket")
+const carriage = document.getElementById("carriage")
+const cpcode = document.getElementById("cpcode")
+const discountElem = document.getElementById("discount")
+const ticketSectionElem = document.getElementById("ticketsection")
 
 sendBtn.addEventListener("click", function () {
     // Raccolta Dati
@@ -28,30 +19,35 @@ sendBtn.addEventListener("click", function () {
     console.log(distanceValue, ageValue, typeof distanceValue, typeof ageValue);
 
     // Logica
-    totalPrice = distanceValue * ticketPrice
-    message = `Il prezzo del biglietto è € ${totalPrice}`
-    console.log(message)
-    document.getElementById("greeting").innerHTML += `Ciao ${usernameValue}!` 
-    document.getElementById("ticket").innerHTML = message
+    const startPrice = distanceValue * ticketPrice
 
-    if (ageValue < 18) 
-    {
-        totalPrice20 = totalPrice - (totalPrice * 0.20);
-        message = `Il prezzo del biglietto è € ${totalPrice20.toFixed(2)}`
-        console.log(message);
-        document.getElementById("ticket").innerHTML = message
+    let discount = 0; 
+
+    if (ageValue < 18) {
+        discount = startPrice * 0.2;
     } else if (ageValue > 65) {
-        totalPrice40 = totalPrice - (totalPrice * 0.40);
-        message = `Il prezzo del biglietto è € ${totalPrice40.toFixed(2)}`
-        console.log(message);
-        document.getElementById("ticket").innerHTML = message
+        discount = startPrice * 0.4;
     }
+    
+    const discountPrice = startPrice - discount;
+    console.log(discountPrice)
+
+    const carriageNum = (Math.floor(Math.random() * 25) + 1)
+
+    const cpcodeNum = (Math.floor(Math.random() * (9999 - 1111 + 1) + 1111))
+    
+    greeting.innerHTML = `${usernameValue}`
+    ticket.innerHTML = `${discountPrice.toFixed(2)}`
+    discountElem.innerHTML = `${discount.toFixed(2)}`
+    carriage.innerHTML = `${carriageNum}`
+    cpcode.innerHTML = `${cpcodeNum}`
+    ticketSectionElem.classList.remove("d-none")
 })
 
 delBtn.addEventListener("click", function () {
     distance.value = "";
     age.value = "";
     username.value = "";
-    document.getElementById("greeting").innerHTML = "";
-    document.getElementById("ticket").innerHTML = "";
+
+    ticketSectionElem.classList.add("d-none")
 })
